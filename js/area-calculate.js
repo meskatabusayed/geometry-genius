d2fdf3f7252a3b4090dcee32cc4b8c4ad5b43faa
meskatabusayed <meskatabusayed@gmail.com>
2024-04-28 13:07:01 +0600

    triangle area calculator part added

--- js/area-calculate.js
+++ js/area-calculate.js
@@ -1,4 +1,4 @@
-function areaCalculator(){
+function triangleAreaCalculator(){
     const triangleBase = document.getElementById('triangle-base');
     const triangleValueText = triangleBase.value;
     const triangleValueNumber = parseFloat(triangleValueText);
@@ -6,5 +6,42 @@ function areaCalculator(){
     const triangleHeight = document.getElementById('triangle-height');
     const triangleHeightText = triangleHeight.value;
     const triangleHeightNumber = parseFloat(triangleHeightText);
-    console.log(triangleHeightNumber);
+
+    const currentTriangleArea = 0.5 * triangleValueNumber * triangleHeightNumber;
+    
+    const triangleArea = document.getElementById('triangle-area');
+    triangleArea.innerText = currentTriangleArea;
+
 }
+
+function rectangleAreaCalculator(){
+    const rectangleWidth = document.getElementById('rectangle-width');
+    const rectangleWidthString = rectangleWidth.value;
+    const width = parseFloat(rectangleWidthString);
+
+    const rectangleLength = document.getElementById('rectangle-length');
+    const rectangleLengthString = rectangleLength.value;
+    const length = parseFloat(rectangleLengthString);
+
+    const rectangleTotalArea = width * length;
+
+    const rectangleCurrentArea = document.getElementById('rectangle-current-area');
+    rectangleCurrentArea.innerText = rectangleTotalArea;
+}
+
+function ParallelogramAreaCalculator(){
+    const base = fieldInput('Parallelogram-base');
+    const height = fieldInput('Parallelogram-height');
+    const area = base * height;
+    console.log(area);
+    
+    
+}
+
+function fieldInput(fieldId){
+    const field = document.getElementById(fieldId);
+    const fieldValueString = field.value;
+    const fieldValueNumber = parseFloat(fieldValueString);
+    return fieldValueNumber;
+}
+
