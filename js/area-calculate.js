65de9baead5ff9e6775b9cb3be86ef6a90d56d6b
meskatabusayed <meskatabusayed@gmail.com>
2024-04-30 14:02:38 +0600

    complete geometry-genius

--- js/area-calculate.js
+++ js/area-calculate.js
@@ -8,6 +8,15 @@ function triangleAreaCalculator(){
     const triangleHeightNumber = parseFloat(triangleHeightText);
 
     const currentTriangleArea = 0.5 * triangleValueNumber * triangleHeightNumber;
+    // validation
+    if(isNaN(triangleValueNumber) || isNaN(triangleHeightNumber)){
+        alert('Please input Number!!');
+        return;
+    }
+
+    calculationEntry('Triangle' , currentTriangleArea);
+
+    
     
     const triangleArea = document.getElementById('triangle-area');
     triangleArea.innerText = currentTriangleArea;
@@ -24,6 +33,7 @@ function rectangleAreaCalculator(){
     const length = parseFloat(rectangleLengthString);
 
     const rectangleTotalArea = width * length;
+    calculationEntry('Rectangle' , rectangleTotalArea);
 
     const rectangleCurrentArea = document.getElementById('rectangle-current-area');
     rectangleCurrentArea.innerText = rectangleTotalArea;
@@ -34,8 +44,17 @@ function ParallelogramAreaCalculator(){
     const height = fieldInput('Parallelogram-height');
     const area = base * height;
     textByInnerText('Parallelogram-current-area' , area);
+    calculationEntry('Parallelogram' , area);
     
     
+}
+
+function ellipseAreaCalculator(){
+    const majorAxis = fieldInput('major-axis');
+    const minorAxis = fieldInput('minor-axis');
+    const area = 3.1416 * majorAxis * minorAxis;
+    textByInnerText('ellipse-currient-area' , area);
+    calculationEntry('Ellipse' , area);
 }
 
 function fieldInput(fieldId){
@@ -49,5 +68,15 @@ function textByInnerText(areaId , area){
     const innerValue = document.getElementById(areaId);
     innerValue.innerText = area;
 
+}
+
+function calculationEntry(areaType, area){
+    
+    const entry = document.getElementById('calculation-entry');
+    const count = entry.childElementCount;
+    const p = document.createElement('p');
+    p.classList.add('my-4');
+    p.innerHTML = `${count + 1}. ${areaType} ${area} cm<sup>2</sup> <button class='btn btn-success'>button</button>`;
+    entry.appendChild(p);
 }
 
